5e0fb0fa5f6f6300f0f4b067eecdbeebe46e1c78
Willi Ye <williye97@gmail.com>
2016-12-10 02:53:03 +0100

    Add op3t

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -99,10 +99,11 @@ void vendor_load_properties() {
 
     property_get("ro.boot.rf_version", rf_version, NULL);
 
-    if (strstr(rf_version, "11") || strstr(rf_version, "31")) {
-        /* Chinese/America*/
+    if (strstr(rf_version, "11") || strstr(rf_version, "12")
+            || strstr(rf_version, "31") || strstr(rf_version, "32")) {
+        /* Chinese/America */
         property_set("ro.product.model", "ONEPLUS A3000");
-    } else if (strstr(rf_version, "21")) {
+    } else if (strstr(rf_version, "21") || strstr(rf_version, "22")) {
         /* Asia/Europe */
         property_set("ro.product.model", "ONEPLUS A3003");
     }
